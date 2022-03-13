632175f8bd05b14a005788bfeebb162d1f148ba7
tunahua <leejunkilowinnive@gmail.com>
2022-03-13 23:04:20 +0800

    專題專案

---
+++ fantaski-fe/src/pages/footer/Joinus.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function joinus() {
+  return (
+    <>
+      <main className="main-bg">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg">12324555</div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default joinus;
